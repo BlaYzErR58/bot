df11342889c63d8dbfd420afd5db3610d08d6a06
BlaYzErR58 <53875053+BlaYzErR58@users.noreply.github.com>
2023-09-24 13:26:52 +0000

    modified:   models/category.py

--- models/category.py
+++ models/category.py
@@ -1,3 +1,21 @@
 # компоненты библиотеки для описания структуры таблицы
-from sqlalchemy import Column, String, Integer, Boolean
-#
+from  sqlalchemy import Column, String, Integer, Boolean
+# класс-конструктор для работы с декларативным стилем работы с SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+ # инициализация декларативного стиля
+ base = declarative_base()
+ 
+ 
+ class Category(Base):
+     # название таблицы
+     __tablename__  = 'caregory'
+     # поля таблицы
+     id = Column(Integer, primary_key=True)
+     name = Column(String, index=True)
+     is_active = Column(Boolean)
+    
+    def  __str__(self):
+        
+        
+        
+        return self.name
